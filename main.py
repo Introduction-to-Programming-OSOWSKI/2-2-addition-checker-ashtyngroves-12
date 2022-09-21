bc4e67db21676a169429398c9c00c92e598b7596
ashtyngroves-12 <817376@mystma.org>
2022-09-21 14:22:14 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#WRITE YOUR CODE HERE
+def greaterThan(x,y):
+    if x > y:
+        return True 
+    else:
+        return False
+def lessThan(x,y):
+    if x < y:
+        return True
+    else:
+        return False
+def equalTo(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+def greaterOrEqual(x,y):
+    if x >= y:
+        return True
+    else:
+        return False
+def lessOrEqual(x,y):
+    if x <= y:
+        return True
+    else:
+        return False 
